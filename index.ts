1425521d9b3e954591a75cc54513281eb949828d
MUHAMMAD Bilal74 <bilalfarooqui74@gmail.com>
2024-03-22 10:37:07 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+let a="your value is"
+
+let ans = await inquirer.prompt([
+  { message: "Please enter a number", type: "number", name: "First_Number" },
+  { message: "Please enter a number", type: "number", name: "Second_Number" },
+  { message: "Please enter any operation you want to perform", type: "list", name: "operator",choices:["add","subtract","multiply","divide"] },
+]);
+
+if (ans.operator==="add"){
+    
+    let b=ans.First_Number + ans.Second_Number
+    console.log(a+" "+ b)
+}else if(ans.operator==="subtract"){
+
+
+    let b=ans.First_Number - ans.Second_Number
+    console.log(a+" "+ b)
+}
+
+else if(ans.operator==="multiply"){
+
+
+    let b=ans.First_Number * ans.Second_Number
+    console.log(a+" "+ b)
+}
+
+else if(ans.operator==="divide"){
+
+
+    let b=ans.First_Number / ans.Second_Number
+    console.log(a+" "+ b)
+}
